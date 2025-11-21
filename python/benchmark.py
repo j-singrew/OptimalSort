e3a8d0669e40d159a0923272a928f62a87c96e57
topshager <rem28777@gmail.com>
2025-11-21 14:40:18 +0200

    refactor benchmark.py

--- python/benchmark.py
+++ python/benchmark.py
@@ -3,18 +3,27 @@ from dataset import DataGeneration
 
 dataset = DataGeneration()
 
+def benchmark_dataset_generation():
+    size_name = ["small","medium","large"]
 
-randomArray  = dataset["random_order"]
-sortedArray = dataset["sorted_order"]
-reversedArray = dataset["reverse_sorted"]
-nearSortedArray = dataset["near_sorted"]
-noDuplicateArray = dataset["no_duplicates"]
-highDuplicateArray = dataset["high_duplicates"]
+    benchmarks_to_run = []
 
+    for pattern_name ,arr_size in dataset.items():
 
-random_Array_small,random_Array_medium,random_Array_large  = randomArray[0] ,randomArray[1] ,randomArray[2]
-sorted_Array_small,sorted_Array_medium,sorted_Array_large = sortedArray[0],sortedArray[1],sortedArray[2]
-reversed_Array_small,reversed_Array_medium,reversed_Array_large =reversedArray[0],reversedArray[1],reversedArray[2]
-nearsorted_Array_Small,nearsorted_Array_medium,nearsorted_Array_large=nearSortedArray[0],nearSortedArray[1],nearSortedArray[2]
-noDuplicateArray_small,noDuplicateArray_medium,noDuplicateArray_large = noDuplicateArray[0],noDuplicateArray[1],noDuplicateArray[2]
-high_Duplpicate_Array_small,high_Duplpicate_Array_medium,high_Duplpicate_Array_large = highDuplicateArray[0],highDuplicateArray[1],highDuplicateArray[2]
+        for i ,arr in enumerate(arr_size):
+            current_size_name = size_name[i]
+
+            arr_benchmark = {
+                "name": f"{pattern_name}_{current_size_name}",
+                "N": len(arr),
+                "data": arr    
+            }
+            benchmarks_to_run .append(arr_benchmark)
+
+    print("--- 18 Test Targets Prepared ---")
+
+    for target in benchmarks_to_run:
+
+        print(f"Target: {target['name']:<25} (N={target['N']:,})")
+        
+    return benchmarks_to_run
