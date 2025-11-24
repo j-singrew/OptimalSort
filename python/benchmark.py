5636767506707d6e2ea5e6c0990bcd85ee29e033
topshager <rem28777@gmail.com>
2025-11-24 21:36:03 +0200

    refactor

--- python/benchmark.py
+++ python/benchmark.py
@@ -55,6 +55,18 @@ def run_c_three_way_quick_sort(arr:np.ndarray):
 
     clibrary.three_way_quick_sort(arr, arr.size)
 
+def run_c_shell_sort(arr:np.ndarray):
+    if clibrary is None:
+        raise RuntimeError("C++ library not loaded. Cannot run C++ 3 shell sort.")   
+
+    clibrary.shell_sort(arr, arr.size)
+
+def run_c_merge_sort(arr:np.ndarray):
+    if clibrary is None:
+        raise RuntimeError("C++ library not loaded. Cannot run C++ merge sort.")   
+        
+    clibrary.merge_sort(arr, arr.size)
+
 def prepare_benchmark_targets():
     size_name = ["small","medium","large"]
 
