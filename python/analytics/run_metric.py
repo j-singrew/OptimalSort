f7f1783431791291966311db5f244e9813b11299
topshager <rem28777@gmail.com>
2026-01-13 22:14:22 +0200

    fix run count bug

--- python/analytics/run_metric.py
+++ python/analytics/run_metric.py
@@ -4,29 +4,39 @@
 def run_variation_counter(dataset:dict) ->dict:
 
     run_data = {}
+
     for key,dataset_tuple in dataset.items():
         runs_per_size = []
 
-        for dataset in dataset_tuple:
-            if not dataset.all():
-                runs_per_size.append(0)
-                continue
+        for data in dataset_tuple:
+            for arr in dataset_tuple:
+                if len(data) < 2:
+                    runs_per_size.append(1)
+                    continue
 
-            current_run_length = 1
-            total_runs = 0
+            runs = 1
+            direction = 0 
 
-            for i in range(1,len(dataset)):
+            for i in range(1,len(data)):
+                if data[i] > data[i-1]:
+                    new_dir = 1
+
+                elif data[i] < data[i -1]:
+                    new_dir = -1
                 
-                if (dataset[i] >= dataset[i-1]):
-                    current_run_length += 1
                 else:
-                    total_runs +=current_run_length
-                    current_run_length = 1
-        
-            total_runs += current_run_length
-            runs_per_size.append(total_runs)
+                    continue
 
-        run_data[key] = runs_per_size
-    print(run_data)
+
+                if direction == 0:
+                    direction = new_dir
+                elif new_dir != direction:
+                    runs +=1
+                    direction = new_dir
+
+                    
+            runs_per_size.append(runs)
+        run_data[key] = runs_per_size   
+        
     return run_data
 
