8891393d1f4764218de05ab004053a31fe05f4fb
topshager <rem28777@gmail.com>
2026-01-14 09:05:09 +0200

    run_metric fix

--- python/analytics/run_metric.py
+++ python/analytics/run_metric.py
@@ -1,5 +1,5 @@
 
-
+#datasets[key] = (arr_small, arr_medium, arr_large)
 
 def run_variation_counter(dataset:dict) ->dict:
 
@@ -8,35 +8,43 @@ def run_variation_counter(dataset:dict) ->dict:
     for key,dataset_tuple in dataset.items():
         runs_per_size = []
 
-        for data in dataset_tuple:
-            for arr in dataset_tuple:
-                if len(data) < 2:
-                    runs_per_size.append(1)
-                    continue
 
-            runs = 1
-            direction = 0 
+        for array_size in dataset_tuple:
 
-            for i in range(1,len(data)):
-                if data[i] > data[i-1]:
-                    new_dir = 1
-
-                elif data[i] < data[i -1]:
-                    new_dir = -1
-                
-                else:
-                    continue
-
-
-                if direction == 0:
-                    direction = new_dir
-                elif new_dir != direction:
-                    runs +=1
-                    direction = new_dir
-
+            data_length = len(array_size)
+            if data_length == 0:
+                runs = 0 
+                normalised_run_metric = 0
+                runs_per_size.append((runs, normalised_run_metric))
+            elif data_length < 2:
                     
-            runs_per_size.append(runs)
-        run_data[key] = runs_per_size   
+                    runs = 1
+                    normalised_run_metric = runs/data_length
+                    runs_per_size.append((runs, normalised_run_metric))
+                    continue
+            else:
+                runs = 1
+                direction = 0 
+                for i in range(1,data_length):
+                    if array_size [i] > array_size [i-1]:
+                        new_dir = 1
+
+                    elif array_size [i] < array_size [i -1]:
+                        new_dir = -1
+                
+                    else:
+                        continue
+
+
+                    if direction == 0:
+                        direction = new_dir
+                    elif new_dir != direction:
+                        runs +=1
+                        direction = new_dir
+
+                normalised_run_metric = runs/data_length
+                runs_per_size.append((runs,normalised_run_metric))
+        run_data[key] = runs_per_size
         
     return run_data
 
